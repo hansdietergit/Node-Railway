304618e5e844c0cf0aed01f3a4be10feef9c8344
hansdietergit <dieterhhans@protonmail.com>
2024-08-20 11:01:54 -0500

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,15 @@
+
+export const POOL = {
+    port: process.env.PORT || '',
+    host: process.env.DB_HOST || '',
+    dbname: process.env.DB_NAME || '',
+    user: process.env.DB_USER || '',
+    pass: process.env.DB_PASSWORD || ''
+}
+
+export const PORT = process.env.PORT || 3000
+export const DB_HOST = process.env.DB_HOST || 'localhost'
+export const DB_USER = process.env.DB_USER || 'root'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'faztpassword'
+export const DB_NAME = process.env.DB_NAME || 'usersdb'
+export const DB_PORT = process.env.DB_PORT || 3306
