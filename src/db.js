da0d46858457744fa51fc2370f3c5fe299289464
hansdietergit <dieterhhans@protonmail.com>
2024-08-20 13:19:14 -0500

    Update db.js

--- src/db.js
+++ src/db.js
@@ -3,7 +3,6 @@ import { POOL } from "./config.js";
 
 export const pool = createPool ({
     host: POOL.host,
-    port: POOL.port,
     user: POOL.user,
     password: POOL.pass,
     database: POOL.dbname
