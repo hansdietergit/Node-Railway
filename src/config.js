f40c291331c47be4228fc106dc7b2e45d6a26d85
hansdietergit <dieterhhans@protonmail.com>
2024-08-20 12:48:41 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,15 +1,15 @@
 
 export const POOL = {
-    port: process.env.DB_PORT || '',
-    host: process.env.DB_HOST || '',
-    dbname: process.env.DB_NAME || '',
-    user: process.env.DB_USER || '',
-    pass: process.env.DB_PASSWORD || ''
+    port: process.env.DB_PORT || '5000',
+    host: process.env.DB_HOST || 'localhost',
+    dbname: process.env.DB_NAME || 'db',
+    user: process.env.DB_USER || 'root',
+    pass: process.env.DB_PASSWORD || 'root'
 }
 
 export const PORT = process.env.PORT || 3000
 export const DB_HOST = process.env.DB_HOST || 'localhost'
 export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'faztpassword'
-export const DB_NAME = process.env.DB_NAME || 'usersdb'
-export const DB_PORT = process.env.DB_PORT || 3306
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'root'
+export const DB_NAME = process.env.DB_NAME || 'db'
+export const DB_PORT = process.env.DB_PORT || 5000
